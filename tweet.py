32cc529c31a82fced9ac1dcbd00769b20ecbee95
Bobby Azarbayejani <bobby@azarbayejani.com>
2021-04-21 19:16:13 -0700

    Use argparse

--- tweet.py
+++ tweet.py
@@ -1,22 +1,21 @@
+import configargparse
 import sys
 import os
 import tweepy
 import urllib.request, json
 
-CONSUMER_KEY = os.environ['CONSUMER_KEY']
-CONSUMER_SECRET = os.environ['CONSUMER_SECRET']
-ACCESS_KEY = os.environ['ACCESS_KEY']
-ACCESS_SECRET = os.environ['ACCESS_SECRET']
+parser = configargparse.ArgParser(description="Tweets the latest soundcloud comment from Soundcloud User")
+parser.add('--twitter_consumer_key', help="Your Twitter app's consumer key", env_var='TWITTER_CONSUMER_KEY', required=True)
+parser.add('--twitter_consumer_secret', help="Your Twitter app's consumer secret", env_var='TWITTER_CONSUMER_SECRET', required=True)
+parser.add('--twitter_access_key', help="Your Twitter app's access key", env_var='TWITTER_ACCESS_KEY', required=True)
+parser.add('--twitter_access_secret', help="Your Twitter app's access secret", env_var='TWITTER_ACCESS_SECRET', required=True)
+parser.add('--soundcloud_client_id', help="A Soundcloud app's client ID", env_var='SOUNDCLOUD_CLIENT_ID', required=True)
+parser.add('soundcloud_user_id', metavar='id', type=int, help="A Soundcloud user's ID")
 
-# You can grab this by looking at Soundcloud API requests in the Network tab of your browser
-SOUNDCLOUD_CLIENT_ID=os.environ['SOUNDCLOUD_CLIENT_ID']
+args = parser.parse_args()
 
-if len(sys.argv) < 2:
-  exit('usage: python tweet.py <soundcloud_user_id>')
-user_id = sys.argv[1]
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+auth = tweepy.OAuthHandler(args.twitter_consumer_key, args.twitter_consumer_secret)
+auth.set_access_token(args.twitter_access_key, args.twitter_access_secret)
 
 api = tweepy.API(auth)
 
@@ -27,7 +26,7 @@ def get_last_tweet_body():
   return tweets[0].text.split('\n')[0]
 
 last_body = get_last_tweet_body()
-soundcloud_api_url = f"https://api-v2.soundcloud.com/users/{user_id}/comments?client_id={SOUNDCLOUD_CLIENT_ID}"
+soundcloud_api_url = f"https://api-v2.soundcloud.com/users/{args.soundcloud_user_id}/comments?client_id={args.soundcloud_client_id}"
 with urllib.request.urlopen(soundcloud_api_url) as url:
   data = json.loads(url.read().decode())
   latest_comment = data['collection'][0]
