48d3965560ebf4cf42e5964930531c8881fc1102
Bobby Azarbayejani <bobby@azarbayejani.com>
2021-06-08 19:59:22 -0700

    Use Puppeteer to scrape

--- tweet_soundcloud_comment.py
+++ tweet_soundcloud_comment.py
@@ -1,16 +1,17 @@
 import configargparse
+import html
+import asyncio
 import sys
 import os
 import tweepy
-import urllib.request, json
+from pyppeteer import launch
 
 parser = configargparse.ArgParser(description="Tweets the latest Soundcloud comment from Soundcloud user ID")
 parser.add('--twitter_consumer_key', help="Your Twitter app's consumer key", env_var='TWITTER_CONSUMER_KEY', required=True)
 parser.add('--twitter_consumer_secret', help="Your Twitter app's consumer secret", env_var='TWITTER_CONSUMER_SECRET', required=True)
 parser.add('--twitter_access_key', help="Your Twitter app's access key", env_var='TWITTER_ACCESS_KEY', required=True)
 parser.add('--twitter_access_secret', help="Your Twitter app's access secret", env_var='TWITTER_ACCESS_SECRET', required=True)
-parser.add('--soundcloud_client_id', help="A Soundcloud app's client ID", env_var='SOUNDCLOUD_CLIENT_ID', required=True)
-parser.add('soundcloud_user_id', metavar='id', type=int, help="A Soundcloud user's ID")
+parser.add('soundcloud_username', metavar='username', type=str, help="A Soundcloud username")
 
 def get_last_tweet():
   tweets = api.user_timeline(id = api.me().id, count = 1, include_entities = True, tweet_mode='extended')
@@ -29,16 +30,27 @@ api = tweepy.API(auth)
 
 last_body, last_url  = get_last_tweet()
 
-soundcloud_api_url = f"https://api-v2.soundcloud.com/users/{args.soundcloud_user_id}/comments?client_id={args.soundcloud_client_id}"
+soundcloud_url = f"http://soundcloud.com/{args.soundcloud_username}/comments"
 
-with urllib.request.urlopen(soundcloud_api_url) as url:
-  data = json.loads(url.read().decode())
-  latest_comment = data['collection'][0]
-  body = latest_comment["body"]
-  permalink = latest_comment["track"]["permalink_url"]
+print(last_body)
+print(last_url)
+
+async def main():
+  browser = await launch({ 'headless': True })
+  page = await browser.newPage()
+  await page.goto(soundcloud_url)
+  await page.waitForSelector('.commentBadge')
+
+  link = await page.querySelector('.commentBadge__title a')
+  permalink = await page.evaluate('(link) => link.href', link)
+  body_container = await page.querySelector('.commentBadge__body')
+  body = await page.evaluate('(body_container) => body_container.innerText', body_container)
+
   output = f"{body}\n{permalink}"
-  if body != last_body or permalink != last_url:
+  if html.escape(body) != last_body or permalink != last_url:
     api.update_status(output)
     print(f"Tweeted: {body}")
   else:
     print('nothing tweeted')
+
+asyncio.get_event_loop().run_until_complete(main())
